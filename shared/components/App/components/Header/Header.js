b5138bbd7ee57ad6f3907dfffe05848c85d0f361
jirrazabal <joseirrazabal@aleani.com.ar>
2017-12-18 00:39:14 -0300

    nav

--- shared/components/App/components/Header/Header.js
+++ shared/components/App/components/Header/Header.js
@@ -11,7 +11,7 @@ import {
   NavbarBrand,
   DropdownToggle,
 } from 'reactstrap';
-import { Link }       from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   constructor(props) {
@@ -60,66 +60,86 @@ class Header extends Component {
           &#9776;
         </NavbarToggler>
 
-        <Nav className="d-md-down-none" navbar>
-          <NavItem className="px-3">
-            <NavLink tag={Link} to="/dashboard">Dashboard</NavLink>
-          </NavItem>
-          <NavItem className="px-3">
-            <NavLink tag={Link} to="#">Users</NavLink>
-          </NavItem>
-          <NavItem className="px-3">
-            <NavLink tag={Link} to="/about">Settings</NavLink>
-          </NavItem>
-        </Nav>
+        {false &&
+          <Nav className="d-md-down-none" navbar>
+            <NavItem className="px-3">
+              <NavLink tag={Link} to="/dashboard">Dashboard</NavLink>
+            </NavItem>
+            <NavItem className="px-3">
+              <NavLink tag={Link} to="#">Users</NavLink>
+            </NavItem>
+            <NavItem className="px-3">
+              <NavLink tag={Link} to="/about">Settings</NavLink>
+            </NavItem>
+          </Nav>
+        }
 
         <Nav className="ml-auto" navbar>
-          <NavItem className="d-md-down-none">
-            <NavLink tag={Link} to="#">
-              <i className="icon-bell" />
-              <Badge pill color="danger">
-                5
-              </Badge>
-            </NavLink>
-          </NavItem>
+          {false &&
+            <NavItem className="d-md-down-none">
+              <NavLink tag={Link} to="#">
+                <i className="icon-bell" />
+                <Badge pill color="danger">
+                  5
+                </Badge>
+              </NavLink>
+            </NavItem>
+          }
 
-          <NavItem className="d-md-down-none">
-            <NavLink tag={Link} to="#">
-              <i className="icon-list" />
-            </NavLink>
-          </NavItem>
+          {false &&
+            <NavItem className="d-md-down-none">
+              <NavLink tag={Link} to="#">
+                <i className="icon-list" />
+              </NavLink>
+            </NavItem>
+          }
 
-          <NavItem className="d-md-down-none">
-            <NavLink tag={Link} to="#">
-              <i className="icon-location-pin" />
-            </NavLink>
-          </NavItem>
+          {false &&
+            <NavItem className="d-md-down-none">
+              <NavLink tag={Link} to="#">
+                <i className="icon-location-pin" />
+              </NavLink>
+            </NavItem>
+          }
 
           <NavItem>
             <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
               <DropdownToggle className="nav-link dropdown-toggle">
-                <img
-                  src={'img/avatars/6.jpg'}
-                  className="img-avatar"
-                  alt="admin@bootstrapmaster.com"
-                />
+                {false &&
+                  <img
+                    src={'img/avatars/6.jpg'}
+                    className="img-avatar"
+                    alt="admin@bootstrapmaster.com"
+                  />
+                }
                 <span className="d-md-down-none">admin</span>
               </DropdownToggle>
               <DropdownMenu right className={this.state.dropdownOpen ? 'show' : ''}>
-                <DropdownItem header tag="div" className="text-center">
-                  <strong>Account</strong>
-                </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-bell-o" /> Updates<Badge color="info">42</Badge>
-                </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-envelope-o" /> Messages<Badge color="success">42</Badge>
-                </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-tasks" /> Tasks<Badge color="danger">42</Badge>
-                </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-comments" /> Comments<Badge color="warning">42</Badge>
-                </DropdownItem>
+                {false &&
+                  <DropdownItem header tag="div" className="text-center">
+                    <strong>Account</strong>
+                  </DropdownItem>
+                }
+                {false &&
+                  <DropdownItem>
+                    <i className="fa fa-bell-o" /> Updates<Badge color="info">42</Badge>
+                  </DropdownItem>
+                }
+                {false &&
+                  <DropdownItem>
+                    <i className="fa fa-envelope-o" /> Messages<Badge color="success">42</Badge>
+                  </DropdownItem>
+                }
+                {false &&
+                  <DropdownItem>
+                    <i className="fa fa-tasks" /> Tasks<Badge color="danger">42</Badge>
+                  </DropdownItem>
+                }
+                {false &&
+                  <DropdownItem>
+                    <i className="fa fa-comments" /> Comments<Badge color="warning">42</Badge>
+                  </DropdownItem>
+                }
                 <DropdownItem header tag="div" className="text-center">
                   <strong>Settings</strong>
                 </DropdownItem>
@@ -129,27 +149,33 @@ class Header extends Component {
                 <DropdownItem>
                   <i className="fa fa-wrench" /> Settings
                 </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-usd" /> Payments<Badge color="secondary">42</Badge>
-                </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-file" /> Projects<Badge color="primary">42</Badge>
-                </DropdownItem>
+                {false &&
+                  <DropdownItem>
+                    <i className="fa fa-usd" /> Payments<Badge color="secondary">42</Badge>
+                  </DropdownItem>
+                }
+                {false &&
+                  <DropdownItem>
+                    <i className="fa fa-file" /> Projects<Badge color="primary">42</Badge>
+                  </DropdownItem>
+                }
                 <DropdownItem divider />
+                {false &&
+                  <DropdownItem>
+                    <i className="fa fa-shield" /> Lock Account
+                  </DropdownItem>
+                }
                 <DropdownItem>
-                  <i className="fa fa-shield" /> Lock Account
-                </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-lock" /> Logout
+                  <NavLink tag={Link} to="/logout"> <i className="fa fa-lock" /> Logout</NavLink>
                 </DropdownItem>
               </DropdownMenu>
             </Dropdown>
           </NavItem>
         </Nav>
-        { false &&
-        <NavbarToggler className="d-md-down-none" type="button" onClick={this.asideToggle}>
-          &#9776;
-        </NavbarToggler>
+        {false &&
+          <NavbarToggler className="d-md-down-none" type="button" onClick={this.asideToggle}>
+            &#9776;
+          </NavbarToggler>
         }
       </header>
     );
